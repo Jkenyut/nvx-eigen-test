949f5ec8db25d87804668776891011ae7bf8c09c
Jkenyut <satrianursaputro06@gmail.com>
2023-05-02 20:33:43 +0700

    update

--- Backend_test_case/routes/routes.js
+++ Backend_test_case/routes/routes.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
   res
     .status(200)
     .send(
-      "enpoint: \n /pinjam  POST : pinjam buku\n /pengembalian POST: pengembalian buku\n /member GET: check member\n /buku GET: check buku\n /data GET :only for generate mocks data\n "
+      "enpoint: \n /pinjam  POST : pinjam buku\n /pengembalian POST: pengembalian buku\n /member GET: check member\n /buku GET: check buku\n /data GET :only for generate mocks data\n /api-docs GET:documentation API Swagger UI"
     );
 });
 router.post("/pinjam", dataControllers.updatePenalty, bookControllers.pinjam);
